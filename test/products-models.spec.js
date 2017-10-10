d4115725a40d5f231b359f7a78d7dcdc1a742b79
Ricky Peters <ericrickyallenpeters@gmail.com>
2017-10-10 11:28:10 -1000

    SuperTests

---
+++ test/products-models.spec.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+const chai = require('chai');
+const expect = chai.expect;
+
+
+Products = require('../db/products');
+
+
+
+describe('Products tests', function () {
+  const products = new Products();
+
+  it('should add a new product to the collection', function () {
+    let result = products.create('Blade Runner', 'DVD');
+    expect(result).to.have('id');
+  });
+
+  it('get product by id', function() {
+    let foundProduct = products.find(3);
+    expect(foundProduct.id).to.equal(3);
+  });
+});
